75bc4ec9d199663a42a0f294bbf752064fef45b7
Baptiste <baptiste.defrance.dev@gmail.com>
2023-02-03 14:04:30 +0100

    Update copy_docker_login.sh

--- copy_docker_login.sh
+++ copy_docker_login.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-mkdir /home/vagrant/.docker/
+mkdir /home/vagrant/.docker/ /root/.docker
 cp /vagrant/docker_config.json /home/vagrant/.docker/config.json
+cp /vagrant/docker_config.json /root/.docker/config.json
 chown -R vagrant: /home/vagrant/.docker
+echo "DOCKER LOGIN FOR vagrant"
 sudo -u vagrant docker login
+echo "DOCKER LOGIN FOR root"
+docker login
