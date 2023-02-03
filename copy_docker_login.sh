4cb43baadd4b3176c17e168093fe8958e55cf7b9
Baptiste <baptiste.defrance.dev@gmail.com>
2023-02-03 14:18:19 +0100

    Update copy_docker_login.sh

--- copy_docker_login.sh
+++ copy_docker_login.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if [ ! -f /vagrant/docker_config.json ]; then
+	echo "No file docker_config.json... Skipping"
+	exit 0
+fi
+
 mkdir /home/vagrant/.docker/ /root/.docker
 cp /vagrant/docker_config.json /home/vagrant/.docker/config.json
 cp /vagrant/docker_config.json /root/.docker/config.json
